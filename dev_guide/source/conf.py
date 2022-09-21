d908174f8e897f26241ed91a50ff935a408a5853
Artem Goncharov <Artem.goncharov@gmail.com>
2022-09-21 09:36:32 +0200

    Update conf.py file (#3)
    Update conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>

--- dev_guide/source/conf.py
+++ dev_guide/source/conf.py
@@ -80,7 +80,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 
-html_title = "Object Storage Service - API Reference"
+html_title = "Object Storage Service - Developer Guide"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -91,7 +91,7 @@ html_static_path = ['_static']
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
 ('index',
-     'obs-api-ref.tex',
-     u'Object Storage Service - API Reference',
+     'obs-dev-guide.tex',
+     u'Object Storage Service - Developer Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
