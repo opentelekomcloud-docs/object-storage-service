e1db08fbcf726bf0148046320f7a2f0b5c6a5d8b
vladimirhasko <vladimirhasko@gmail.com>
2023-02-02 12:02:05 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- doc/swiftapi/source/conf.py
+++ doc/swiftapi/source/conf.py
@@ -81,7 +81,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 
-html_title = "Object Storage Service - S3 API Reference"
+html_title = "Object Storage Service - API Reference (Swift)"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -95,7 +95,7 @@ html_copy_source = False
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
     ('index',
-     'obs-s3api.tex',
-     u'Object Storage Service - S3 API Reference',
+     'obs-swiftapi.tex',
+     u'Object Storage Service - API Reference (Swift)',
      u'OpenTelekomCloud', 'manual'),
 ]
