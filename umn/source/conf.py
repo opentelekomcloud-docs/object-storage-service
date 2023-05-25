73455d0a731bbfc5c5c1d4f120a48d2967500e01
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2023-05-25 10:21:22 +0200

    Update tox.ini && conf.py file (#56)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,6 +28,15 @@ project = 'Object Storage Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/object-storage-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/object-storage-service/umn/'
+otcdocs_doc_title = 'User Guide'
+otcdocs_doc_type = 'umn'
+otcdocs_service_title = 'Object Storage Service'
+otcdocs_service_type = 'obs'
+otcdocs_service_category = 'storage'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
